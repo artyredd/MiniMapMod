882fe065b3d29967fe65660872f31eaf68e3495a
Deku <39616108+DekuDesu@users.noreply.github.com>
2021-08-20 11:25:47 -0400

    hotfix- performance

---
+++ MiniMapMod/ITrackedObject.cs
@@ -0,0 +1,16 @@
+﻿using MiniMapLibrary;
+using UnityEngine;
+
+namespace MiniMapMod
+{
+    public interface ITrackedObject
+    {
+        GameObject gameObject { get; set; }
+        InteractableKind InteractableType { get; set; }
+        RectTransform MinimapTransform { get; set; }
+        bool DynamicObject { get; set; }
+
+        void CheckActive();
+        void Destroy();
+    }
+}
