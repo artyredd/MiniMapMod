36de05a695acbb645b628bbb97f1c396e34d45c5
Deku <39616108+DekuDesu@users.noreply.github.com>
2021-08-21 06:08:29 -0400

    performance minor, refactored position math, refactored tracked object registration

---
+++ MiniMapLibrary/Basic/Range.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniMapLibrary
+{
+    public class Range1D
+    {
+        public float Min { get; set; }
+
+        public float Max { get; set; }
+
+        /// <summary>
+        /// Equivalent to <see cref="Range1D.Max"/> - <see cref="Min"/>
+        /// </summary>
+        public float Difference => Max - Min;
+
+        /// <summary>
+        /// Equivalent to -<see cref="Min"/>(neg)
+        /// </summary>
+        public float Offset => -Min;
+
+        public void CheckValue(float value)
+        {
+            if (value < Min)
+            {
+                Min = value;
+                return;
+            }
+            if (value > Max)
+            {
+                Max = value;
+                return;
+            }
+        }
+
+        public void Clear()
+        {
+            Min = float.MaxValue;
+            Max = float.MinValue;
+        }
+    }
+}
---
+++ MiniMapLibrary/Basic/Range3D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MiniMapLibrary
+{
+    public class Range3D
+    {
+        public Range1D X { get; set; } = new Range1D();
+
+        public Range1D Y { get; set; } = new Range1D();
+
+        public Range1D Z { get; set; } = new Range1D();
+
+        public void CheckValue(Vector3 position)
+        {
+            X.CheckValue(position.x);
+            //Y.CheckValue(position.y);
+            Z.CheckValue(position.z);
+        }
+
+        public void Clear()
+        {
+            X.Clear();
+            Y.Clear();
+            Z.Clear();
+        }
+    }
+}
